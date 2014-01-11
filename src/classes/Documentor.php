4653c267f0743080c5b5bc05a82d9a5c0492b9e9
Stef Horner <stef@tedslittlerobot.com>
2014-01-11 15:42:16 +0000

    set up the main documentor class

---
+++ src/classes/Documentor.php
@@ -0,0 +1,51 @@
+<?php namespace Documentor;
+
+use Documentor\Contract\DocumentInterface;
+use Documentor\Contract\OutputRendererInterface;
+
+class Documentor {
+
+	/**
+	 * The document
+	 * @var Documentor\Contract\DocumentInterface
+	 */
+	protected $document;
+
+	/**
+	 * The output renderer
+	 * @var Documentor\Contract\OutputRendererInterface
+	 */
+	protected $output;
+
+	public function __construct() {}
+
+	/**
+	 * Set the output renderer
+	 * @param  OutputRendererInterface $output
+	 */
+	public function setRenderer( OutputRendererInterface $output )
+	{
+		$this->output = $output;
+		return $this;
+	}
+
+	/**
+	 * Get the output renderer
+	 * @return OutputRendererInterface
+	 */
+	public function getRenderer()
+	{
+		return $this->output;
+	}
+
+	/**
+	 * Render the given document
+	 * @param  DocumentInterface $document
+	 * @return string
+	 */
+	public function render( DocumentInterface $document )
+	{
+		return $this->output->renderDocument( $document->getContent() );
+	}
+
+}
---
+++ tests/TestDocumentor.php
@@ -0,0 +1,40 @@
+<?php
+
+use Mockery as m;
+
+class TestDocumentor extends \PHPUnit_Framework_TestCase {
+
+	public function setUp()
+	{
+		$this->documentor = new Documentor\Documentor;
+		$this->document = m::mock('Documentor\Contract\DocumentInterface');
+		$this->output = m::mock('Documentor\Contract\OutputRendererInterface');
+
+		$this->documentor->setRenderer( $this->output );
+	}
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * Assert that the renderer has been assigned properly
+	 */
+	public function testRendererAssignment()
+	{
+		$this->assertSame( $this->documentor->getRenderer(), $this->output );
+	}
+
+	/**
+	 * Assert that the render methods are called
+	 */
+	public function testRender()
+	{
+		$this->document->shouldReceive('getContent')->once()->andReturn('foo');
+		$this->output->shouldReceive('renderDocument')->once()->with('foo')->andReturn('woop');
+
+		$this->assertEquals( 'woop', $this->documentor->render( $this->document ) );
+	}
+
+}
